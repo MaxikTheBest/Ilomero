90c6e35c4fe49e18e0ed7a2dc01b3485bb6e43d7
MaxikTheBest <maxik.uhnak@gmail.com>
2025-05-23 10:44:19 +0200

    Add files via upload

---
+++ api/windows/oldexplorer.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirecting...</title>
+  <script>
+    window.location.href = "https://ilomero.com/api/windows/oldexplorer.html";
+  </script>
+</head>
+<body>
+  <p>Redirecting you...</p>
+</body>
+</html>
