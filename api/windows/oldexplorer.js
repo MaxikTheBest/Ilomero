984686ca4b6205e7c3b9b3a340ce6c6306c0a465
MaxikTheBest <maxik.uhnak@gmail.com>
2025-05-23 10:55:25 +0200

    Update oldexplorer.js

--- api/windows/oldexplorer.js
+++ api/windows/oldexplorer.js
@@ -1,12 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta http-equiv="refresh" content="3; URL=https://ilomero.com/api/windows/oldexplorer.html">
   <title>Redirecting...</title>
-  <script>
-    window.location.href = "https://ilomero.com/api/windows/oldexplorer.html";
-  </script>
+  <style type="text/css">
+    body {
+      background-color: #c0c0c0;
+      font-family: "MS Sans Serif", Tahoma, Arial;
+      color: #000080;
+      text-align: center;
+      margin: 0;
+      padding: 0;
+    }
+    .window {
+      border: 2px solid #000080;
+      background-color: #e0e0e0;
+      width: 400px;
+      margin: 100px auto;
+      padding: 10px;
+    }
+    .title-bar {
+      background-color: #000080;
+      color: white;
+      font-weight: bold;
+      padding: 4px;
+      text-align: left;
+      font-size: 14px;
+    }
+    .content {
+      background-color: #ffffff;
+      padding: 20px;
+      border-top: 2px solid #808080;
+    }
+  </style>
 </head>
 <body>
-  <p>Redirecting you...</p>
+
+<div class="window">
+  <div class="title-bar">Microsoft Internet Explorer</div>
+  <div class="content">
+    <p>Opening microsoft old browser in a new window...</p>
+    <p><small>If it hasnt poped up in your taskbar, click here:, <a href="https://ilomero.com/api/windows/oldexplorer.html">click here</a>.</small></p>
+  </div>
+</div>
+
 </body>
 </html>
